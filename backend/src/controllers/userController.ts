51b2d5ca01d2db4b02e62897af1e0cc00eb690b2
LizzColDev <lquintero28@live.com>
2023-08-03 14:10:36 -0500

    Added interface for keyData object

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,10 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
 import createError from 'http-errors';
-import admin from 'firebase-admin';
-const credentials = require('../../key.json');
+import * as admin from 'firebase-admin';
+import keyData from '../../key.json';
+import { generateCustomId } from '../utils/customIds';
+
+const params = {
+  type: keyData.type,
+  projectId: keyData.project_id,
+  privateKeyId: keyData.private_key_id,
+  privateKey: keyData.private_key,
+  clientEmail: keyData.client_email,
+  clientId: keyData.client_id,
+  authUri: keyData.auth_uri,
+  tokenUri: keyData.token_uri,
+  authProviderX509CertUrl: keyData.auth_provider_x509_cert_url,
+  clientC509CertUrl: keyData.client_x509_cert_url
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(credentials)
+  credential: admin.credential.cert(params)
 });
 
 const db = admin.firestore();
@@ -16,13 +30,15 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     if (!body.name || typeof body.name !== 'string' || body.name.trim().length === 0) {
       return next(createError(422, 'Invalid name. Name must be a non-empty string.'));
     }
+
+    const customUserId = generateCustomId()
+
     const newUser = {
-      userId: 0,
+      userId: customUserId,
       name: body.name.trim(),
     };
 
-    const docRef =  await db.collection('users').add(newUser);
-    newUser.userId = Number(docRef.id);
+    await db.collection('users').doc(customUserId).set(newUser);
 
     res.status(201).json(newUser);
   } catch (error) {
