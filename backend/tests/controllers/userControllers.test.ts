bbd8d3ac2b1e457f4683a6a20d4981b202153304
LizzColDev <lquintero28@live.com>
2023-08-05 17:02:59 -0500

    Replace firestore-jest-mock with native Jest mock

--- backend/tests/controllers/userControllers.test.ts
+++ backend/tests/controllers/userControllers.test.ts
@@ -1,46 +1,61 @@
-import { Request, Response, NextFunction } from 'express';
-import { mockFirebase   } from 'firestore-jest-mock';
-import { mockCollection } from 'firestore-jest-mock/mocks/firestore';
+import { Request, Response, NextFunction } from "express";
+import { createUser } from "../../src/controllers/userController";
+import createError from 'http-errors';
 
-mockFirebase();
-mockCollection('users');
+jest.mock("firebase-admin", () => ({
+  ...jest.mock("firebase-admin"),
+  credential: {
+    cert: jest.fn(),
+  },
+  initializeApp: jest.fn(),
+  firestore: () => ({
+    collection: () => ({
+      add: (data: object) => {
+        return Promise.resolve({ id: "1", username: "Samantha" });
+      },
+    }),
+  }),
+}));
 
-import { createUser } from '../../src/controllers/userController';
-
-describe('POST /users', () => {
+describe("User Controller - POST /users", () => {
   let req: Request;
   let res: Response;
   let next: NextFunction;
 
   beforeEach(() => {
-req={
-  body: {
-    name: 'Pepita'
-  }
-} as Request;
+    req = {
+      body: {
+        name: "Samantha",
+      },
+    } as Request;
 
-res = {
-  status: jest.fn().mockReturnThis(),
-  json: jest.fn(),
-} as unknown as Response;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
 
- next = jest.fn();
-
+    next = jest.fn();
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should create a user', async () => {
+  it("should create a new user in Firebase with valid data", async () => {
+    await createUser(req, res, next);
 
-      await createUser(req, res, next);
-
-    
     expect(res.status).toHaveBeenCalledWith(201);
-    expect(res.json).toHaveBeenCalledWith(expect.objectContaining({ name: 'Pepita' }));
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({ id: "1", name: "Samantha" })
+    );
+  });
 
-    expect(mockCollection).toHaveBeenCalledWith('users');
+  it("should return 422 status and error message for invalid username (empty)", async () => {
     
+    req.body.name = '';
+
+    await createUser(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(createError(422, 'Invalid name. Name must be a non-empty string.'));
   });
 });
