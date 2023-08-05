3c6e5f999899bfdae0034fbaae0ea2ca684c69f2
LizzColDev <lquintero28@live.com>
2023-08-05 17:32:16 -0500

    Simplified Firebase key initialization

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,23 +1,10 @@
-import { NextFunction, Request, Response } from 'express';
-import createError from 'http-errors';
-import * as admin from 'firebase-admin';
-import keyData from '../../key.json';
+import { NextFunction, Request, Response } from "express";
+import createError from "http-errors";
+import * as admin from "firebase-admin";
 
-const params = {
-  type: keyData.type,
-  projectId: keyData.project_id,
-  privateKeyId: keyData.private_key_id,
-  privateKey: keyData.private_key,
-  clientEmail: keyData.client_email,
-  clientId: keyData.client_id,
-  authUri: keyData.auth_uri,
-  tokenUri: keyData.token_uri,
-  authProviderX509CertUrl: keyData.auth_provider_x509_cert_url,
-  clientC509CertUrl: keyData.client_x509_cert_url
-}
-
+const FIREBASE_KEY_JSON_PATH = process.env.FIREBASE_KEY_JSON_PATH || "/app/key.json";
 admin.initializeApp({
-  credential: admin.credential.cert(params)
+  credential: admin.credential.cert(FIREBASE_KEY_JSON_PATH),
 });
 
 const db = admin.firestore();
@@ -25,17 +12,22 @@ const db = admin.firestore();
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { body } = req;
-    
-    if (!body.name || typeof body.name !== 'string' || body.name.trim().length === 0) {
-      return next(createError(422, 'Invalid name. Name must be a non-empty string.'));
-    }
 
+    if (
+      !body.name ||
+      typeof body.name !== "string" ||
+      body.name.trim().length === 0
+    ) {
+      return next(
+        createError(422, "Invalid name. Name must be a non-empty string.")
+      );
+    }
 
     const newUser = {
       name: body.name.trim(),
     };
 
-    const documentREference = await db.collection('users').add(newUser);
+    const documentREference = await db.collection("users").add(newUser);
 
     res.status(201).json({
       id: documentREference.id,
@@ -45,6 +37,5 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 };
-
 
 export { createUser };
