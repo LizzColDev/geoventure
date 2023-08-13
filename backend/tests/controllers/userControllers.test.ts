94dc10dc112e02453a0af858437ef147ae655375
LizzColDev <lquintero28@live.com>
2023-08-13 18:58:07 -0500

    Added test for userController getUserById

--- backend/tests/controllers/userControllers.test.ts
+++ backend/tests/controllers/userControllers.test.ts
@@ -1,37 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-import { createUser, getUserById, getUsers } from "../../src/controllers/userController";
-import createError from 'http-errors';
+import {
+  createUser,
+  getUserById,
+  getUsers,
+} from "../../src/controllers/userController";
+import createError from "http-errors";
 
 let req: Request;
 let res: Response;
 let next: NextFunction;
-
-// Mocking the Firestore 'add' function with a resolved promise
-const addMock = jest.fn((data: object) => {
-  return Promise.resolve({ id: "1", username: "Samantha" });
-});
-
-const docsMock = jest.fn((data: object) => {
-  return Promise.resolve([
-    { id: 'user1', userName: 'Test User 1'},
-    { id: 'user2', userName: 'Test User 2'}
-  ]);
-});
-
-    
-const getMock = jest.fn(() => ({
-  forEach: (callback: (value: any, index: number, array: any[]) => void) => {
-    const mockUsers = [
-      { id: 'user1', userName: 'Test User 1' },
-      { id: 'user2', userName: 'Test User 2' }
-    ];
-
-    // Loop through the mockUsers array and execute the callback for each user
-    mockUsers.forEach((user, index) => {
-      callback({ data: () => user, id: `user${index + 1}` }, index, mockUsers);
-    });
-  }
-}));
 
 jest.mock("firebase-admin", () => ({
   ...jest.mock("firebase-admin"),
@@ -41,16 +18,57 @@ jest.mock("firebase-admin", () => ({
   initializeApp: jest.fn(),
   firestore: () => ({
     collection: () => ({
-      docs: docsMock ,
+      docs: docsMock,
       add: addMock,
       get: getMock,
+      doc: getUserByIdMock,
     }),
   }),
 }));
 
-describe("User Controller - POST /users", () => {
+// Mocking the Firestore 'add' function with a resolved promise
+const addMock = jest.fn((data: object) => {
+  return Promise.resolve({ id: "1", username: "Samantha" });
+});
 
+const docsMock = jest.fn((data: object) => {
+  return Promise.resolve([
+    { id: "user1", userName: "Test User 1" },
+    { id: "user2", userName: "Test User 2" },
+  ]);
+});
+
+const getMock = jest.fn(() => ({
+  forEach: (
+    callback: (value: any, index: number, array: any[]) => void
+  ) => {
+    const mockUsers = [
+      { id: "user1", userName: "Test User 1" },
+      { id: "user2", userName: "Test User 2" },
+    ];
+
+    // Loop through the mockUsers array and execute the callback for each user
+    mockUsers.forEach((user, index) => {
+      callback(
+        { data: () => user, id: `user${index + 1}` },
+        index,
+        mockUsers
+      );
+    });
+  },
+}));
+
+let getUserByIdMock = (userId: string,) => ({
+  get: async () => ({
+    exists: userId === "user1",
+    id: userId,
+    data: () => ({ name: "Juanita Test" }),
+  }),
+});
+
+describe("User Controller - POST /users", () => {
   beforeEach(() => {
+
     req = {
       body: {
         name: "Samantha",
@@ -82,17 +100,17 @@ describe("User Controller - POST /users", () => {
   });
 
   it("should return 422 status and error message for invalid username (empty)", async () => {
-    
-    req.body.name = '';
+    req.body.name = "";
 
     await createUser(req, res, next);
 
     // Assert that 'next' is called with the appropriate error
-    expect(next).toHaveBeenCalledWith(createError(422, 'Invalid name. Name must be a non-empty string.'));
+    expect(next).toHaveBeenCalledWith(
+      createError(422, "Invalid name. Name must be a non-empty string.")
+    );
   });
 
   it("should handle database error and call next with the error", async () => {
-
     const errorMock = new Error("Database error");
 
     // Mock the 'add' function to return a rejected promise with the error
@@ -103,11 +121,9 @@ describe("User Controller - POST /users", () => {
     // Assert that 'next' is called with the database error
     expect(next).toHaveBeenCalledWith(errorMock);
   });
-
 });
 
-describe('GET /users', () => {
-
+describe("GET /users", () => {
   const mockResponse = (): Response => {
     const response: Partial<Response> = {};
     response.status = jest.fn().mockReturnValue(response);
@@ -126,46 +142,50 @@ describe('GET /users', () => {
   });
 
   it("should respond with an array of all users in firebase", async () => {
-
     await getUsers(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith([
-      { id: 'user1', userName: 'Test User 1'},
-      { id: 'user2', userName: 'Test User 2'}
+      { id: "user1", userName: "Test User 1" },
+      { id: "user2", userName: "Test User 2" },
     ]);
   });
 
-  it("should respond with 500 and an error message for a server error", async () => {
-
-  });
+  it("should respond with 500 and an error message for a server error", async () => {});
 });
 
 describe("Users Controllers - GET /users/:userId", () => {
+  beforeEach(() => {
+    req = {
+      params: {},
+    } as unknown as Request;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    next = jest.fn() as NextFunction;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should respond with the user details in firebase", async () => {
-    console.log("res object before calling getUserById:", res);
+    req.params.userId = "user1"
     await getUserById(req, res, next);
-    console.log("res object after calling getUserById:", res);
-  
-    console.log("res.status mock:", res.status);
-  
-    const statusMock = res.status as jest.MockedFunction<typeof res.status>;
-    const jsonMock = res.json as jest.MockedFunction<typeof res.json>;
-  
-    console.log("statusMock.mock.calls:", statusMock.mock.calls);
-    console.log("jsonMock.mock.calls:", jsonMock.mock.calls);
-  
-    expect(statusMock).toHaveBeenCalledWith(200);
-    expect(jsonMock).toHaveBeenCalledWith({ id: 'user1', userName: 'Juana Test' });
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      id: "user1",
+      name: "Juanita Test",
+    });
     expect(next).not.toHaveBeenCalled();
   });
 
-  it("should respond with an error and a 500 status code for user retrieval failure", async () => {
-    await getUserById(req, res, next);
-
-  });
-
   it("should respond with a 404 status code for user not found", async () => {
+
     await getUserById(req, res, next);
+
   });
 });
