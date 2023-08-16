cd7f8d0a2efe9d7c996328cc063732ad36b2c534
LizzColDev <lquintero28@live.com>
2023-08-16 17:01:52 -0500

    Added initial test for gamesController

---
+++ backend/tests/controllers/gamesControllers.test.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+let req: Request;
+let res: Response;
+let next: NextFunction;
+
+describe("Games Controller - POST /games", () => {
+
+    it('Should create a new game', async () => {
+
+    });
+})
