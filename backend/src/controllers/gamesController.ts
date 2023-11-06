87998de1e0fd1ba39853f6c3b87f11019afb1279
Lisbeth Quintero <lizzcoldev@gmail.com>
2023-11-06 16:28:30 -0500

    Added user existence check in createGame

--- backend/src/controllers/gamesController.ts
+++ backend/src/controllers/gamesController.ts
@@ -6,6 +6,18 @@ const db = admin.firestore();
 export const createGame = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { userId } = req.body;
+
+    if (userId === undefined || userId === "") {
+      return res.status(400).json({ error: 'userId is required' });
+    }
+
+    const userRef = admin.firestore().collection('users').doc(userId);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
     const currentTime = new Date().getTime()
 
     const gameData = {
@@ -13,10 +25,11 @@ export const createGame = async (req: Request, res: Response, next: NextFunction
       initialTime: currentTime,
     };
 
-    const gameRef = db.collection("users").doc();
-    await gameRef.set(gameData);
-
-    res.status(201).send(gameData);
+    const gameRef = await db.collection("games").add(gameData);
+    res.status(201).send({
+      gameId: gameRef.id,
+      ...gameData,
+    });
 
   } catch (error) {
     next(error);
