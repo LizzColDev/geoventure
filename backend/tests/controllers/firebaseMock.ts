0bc91cb4497363a1ba3c15f200f2a4248d4292a8
Lisbeth Quintero <lizzcoldev@gmail.com>
2023-12-18 14:23:50 -0500

    Implement initial TDD test for getGames in gamesController

--- backend/tests/controllers/firebaseMock.ts
+++ backend/tests/controllers/firebaseMock.ts
@@ -7,7 +7,7 @@ export const createFirebaseMock = () => ({
   firestore: () => ({
     collection: (collectionName: string) => ({
       add: collectionName === "users" ? addMock : addGameMock,
-      get: collectionName === "users" ? getMock : getMock,
+      get: getMock.bind(null, collectionName),
       doc: (docId: string) => ({
         get: getUserByIdMock.bind(null, docId),
         delete: deleteMock,
@@ -21,19 +21,42 @@ export const addMock = jest.fn((data: object) => {
   return Promise.resolve({ id: "1", username: "Samantha" });
 });
 
-export const getMock = jest.fn(() => ({
-  forEach: (callback: (value: any, index: number, array: any[]) => void) => {
-    const mockUsers = [
-      { id: "user1", userName: "Test User 1" },
-      { id: "user2", userName: "Test User 2" },
-    ];
+export const getMock = jest.fn(async (collectionName) => {
+  return Promise.resolve({
+    forEach: (callback: (value: any, index: number, array: any[]) => void) => {
+      let mockData: any[] = []
+      if(collectionName === "users"){
+        const users = [
+          { id: "user1", userName: "Test User 1" },
+          { id: "user2", userName: "Test User 2" },
+        ];
+        mockData = users;
+      }
+      else if(collectionName === "games"){
+        const games = [
+          {
+            id: "gameId1",
+            initialTime: 123,
+            userId: "test user id 1"
+          },
+          {		
+            id: "gameId2",
+            initialTime: 1234,
+            userId: "test user id 2"
+        },
+        ];
+        mockData = games;
+      }
+  
+      // Loop through the mockData array and execute the callback for each user or game
+      mockData.forEach((dataItem, index) => {
+        callback({ data: () => dataItem, id: `test${index + 1}` }, index, mockData);
+      });
+    },
+  })
+  
 
-    // Loop through the mockUsers array and execute the callback for each user
-    mockUsers.forEach((user, index) => {
-      callback({ data: () => user, id: `user${index + 1}` }, index, mockUsers);
-    });
-  },
-}));
+});
 
 // Creating a mock for Firebase Firestore operations in the context of retrieving user data
 export const getUserByIdMock =  jest.fn(async (userId: string) => {
