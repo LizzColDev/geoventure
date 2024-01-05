76580d8bb47e022a213fa2a10a6e9eaae73da46b
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-01-04 23:45:28 -0500

    Updated Docker configuration for Firebase credentials

--- fastApi/database/firestore.py
+++ fastApi/database/firestore.py
@@ -1,9 +1,10 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from fastapi import HTTPException
 
-cred = credentials.Certificate('./key.json')
+cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
