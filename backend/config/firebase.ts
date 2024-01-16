7ca4597cb8858e618de1e31d7f8e3e65d512f568
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-01-15 19:58:20 -0500

    Refactored import in config/firebase.ts

--- backend/config/firebase.ts
+++ backend/config/firebase.ts
@@ -1,9 +1,8 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../key.json';
 
 const serviceAccountKey = process.env.FIREBASE_CREDENTIALS
   ? JSON.parse(process.env.FIREBASE_CREDENTIALS)
-  : serviceAccount;
+  : undefined;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey as admin.ServiceAccount),
