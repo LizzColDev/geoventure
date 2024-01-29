5ccd857f6ed95203fcedc0efdd5beb540a8923f0
lizzcoldev <lizzcoldev@gmail.com>
2024-01-28 22:09:55 -0500

    Created Coordinates interface and updated GameData

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,21 +1,23 @@
+export interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+export interface StreetViewOptions extends Coordinates {}
+
 export interface GameData {
-	exists: boolean;
-	id: string;
-	endTime: number;
-	initialTime: number;
-	userId: string;
-	initialLocation?: {
-	latitude: number;
-	longitude: number;
-	};
-	guessedLocation?: {
-			latitude: number;
-			longitude: number;
-	};
+  exists: boolean;
+  id: string;
+  endTime: number;
+  initialTime: number;
+  userId: string;
+  initialLocation?: Coordinates;
+  guessedLocation?: Coordinates;
+	streetViewImage?: string;
 }
 
 export interface UserData {
-	exists: boolean;
-	id: string;
-	name: string;
+  exists: boolean;
+  id: string;
+  name: string;
 }
