5ed3cf1156d08135277953a50273a073c2d70df1
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-03-19 11:35:06 +0100

    Implemented Google Places API query function

--- backend/src/services/streetviewService.ts
+++ backend/src/services/streetviewService.ts
@@ -1,12 +1,54 @@
 import axios, { AxiosResponse } from "axios";
-import { StreetViewOptions } from "../types";
+import { Coordinates, StreetViewInfo } from "../types";
+import { getRandomLocation } from "../utils/location/generatedRandomLocation";
 
 const GOOGLE_MAPS_API_KEY: string = process.env.GOOGLE_MAPS_API_KEY as string;
 
-// Function to get a random Street View image of a famous place
-const  getStreetViewImage =async (options: StreetViewOptions): Promise<string> => {
+
+// Function to search for famous places using Google Places API
+const searchFamousPlace = async (): Promise<StreetViewInfo[]> => {
   try {
-    const { latitude, longitude } = options;
+    const response = await axios.post(
+      'https://places.googleapis.com/v1/places:searchText',
+      {
+        textQuery: 'tourist_attraction in the world',
+        rankPreference: 'RELEVANCE',
+        minRating: 4.5,
+        maxResultCount: 10
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Goog-Api-Key': GOOGLE_MAPS_API_KEY,
+          'X-Goog-FieldMask': 'places.displayName,places.location,places.types,places.viewport',
+        },
+        
+      }
+    );
+
+    const famousPlaces: StreetViewInfo[] = response.data.places?.map((
+      place: { location: Coordinates, displayName: { text: string } }
+    ) => ({
+      initialLocation: place.location,
+      namePlace: place.displayName.text
+    })) || [];
+
+    return famousPlaces;
+
+  } catch (error: unknown) {
+    console.error('Error fetching places', error);
+    throw new Error('Error fetching places.');
+  }
+}
+
+// Function to get a random Street View image of a famous place
+const  getStreetViewImage =async (): Promise<StreetViewInfo> => {
+  try {
+    const uniqueLocations = await searchFamousPlace();
+
+    const randomLocation = getRandomLocation(uniqueLocations);
+    const { initialLocation, namePlace } = randomLocation;
+    const { latitude, longitude } = initialLocation;
     const size = '600x300'
     const heading = '0';
     const streetViewUrl = 
@@ -19,8 +61,7 @@ const  getStreetViewImage =async (options: StreetViewOptions): Promise<string> =
     if (response.status !== 200) {
       throw new Error(`Error fetching StreetView image. Status code: ${response.status}`);
     }
-    
-    return streetViewImage;
+    return { urlImage: streetViewImage, initialLocation: initialLocation, namePlace: namePlace };
 
   } catch (error:unknown) {
     if (axios.isAxiosError(error)) {
