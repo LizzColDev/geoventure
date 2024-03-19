a89c302d504299173cb549f53ef8ff53a855b178
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-03-19 11:21:24 +0100

    Updated location retrieval method and implemented error checking

--- backend/src/utils/location/generatedRandomLocation.ts
+++ backend/src/utils/location/generatedRandomLocation.ts
@@ -1,18 +1,22 @@
-export const generateRandomLocation =async () => {
- const minLatitude = -90;
- const maxLatitude = 90;
- const minLongitude = -180;
- const maxLongitude = 180; 
+import { StreetViewInfo } from "../../types";
 
- const randomLatitude = getRandomNumber(minLatitude, maxLatitude);
- const randomLongitude = getRandomNumber(minLongitude, maxLongitude);
+let currentIndex = 0;
 
- return {
-  latitude: randomLatitude,
-  longitude: randomLongitude,
- };
-};
+export const getRandomLocation = (locations: StreetViewInfo[]): StreetViewInfo => {
+  // Check if the locations array is empty
+  if (locations.length === 0) {
+    throw new Error('The list of locations is empty.');
+  }
 
-const getRandomNumber = (min: number, max: number) =>{
-  return Math.random() * ( max - min ) + min;
-};
+  try {
+    // Get the current location
+    const currentLocation = locations[currentIndex];
+    // Move to the next location
+    currentIndex = (currentIndex + 1) % locations.length;
+    
+    return currentLocation;
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Error fetching the random location.');
+  }
+}
