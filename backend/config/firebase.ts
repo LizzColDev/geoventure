15bb10ac8b7018c3d6febb41325e9361ff8df49f
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-04-08 23:57:05 +0200

    Configured Firebase emulator environment with Docker Compose

--- backend/config/firebase.ts
+++ backend/config/firebase.ts
@@ -1,30 +1,35 @@
-    import * as path from 'path';
-    import * as admin from 'firebase-admin';
+import * as path from 'path';
+import * as admin from 'firebase-admin';
 
-    const getFirebaseCredentials = () => {
-      
-      if (process.env.FIREBASE_CREDENTIALS) { 
-        return JSON.parse(process.env.FIREBASE_CREDENTIALS);
-      } else {
-        // Path to the key.json file for GitHub Actions
-        const keyJsonPath = path.resolve(__dirname, '../key.json');
-        
-        try {
-          // Attempt to load from key.json locally
-          return require(keyJsonPath);
-        } catch (error) {
-          console.error(`Error loading Firebase credentials from ${keyJsonPath}:`, error);
-          return null;
-        }
-      }
-    };
-
-    const serviceAccountKey = getFirebaseCredentials();
-
-    if (serviceAccountKey) {
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountKey as admin.ServiceAccount),
-      });
+// Function to retrieve Firebase credentials based on environment variables or key file
+const getFirebaseCredentials = () => {
+  if (process.env.FIREBASE_CREDENTIALS) { 
+    // Parse credentials from environment variable
+    return JSON.parse(process.env.FIREBASE_CREDENTIALS);
+  } else if (process.env.FIREBASE_EMULATOR_HOST) {
+    // Using the emulator, no real credentials needed
+    return null;
+  } else {
+    // Load credentials from key.json file
+    const keyJsonPath = path.resolve(__dirname, '../key.json');
+    try {
+      return require(keyJsonPath);
+    } catch (error) {
+      console.error(`Error loading Firebase credentials from ${keyJsonPath}:`, error);
+      return null;
     }
+  }
+};
 
-    export default admin;
+const serviceAccountKey = getFirebaseCredentials();
+
+if (serviceAccountKey || process.env.FIREBASE_EMULATOR_HOST) {
+  // Initialize Firebase Admin SDK with credentials or emulator settings
+  admin.initializeApp({
+    credential: serviceAccountKey ?
+      admin.credential.cert(serviceAccountKey as admin.ServiceAccount) :
+      admin.credential.applicationDefault(), 
+  });
+}
+
+export default admin;
