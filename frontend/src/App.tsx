6737da3ef21673f81dd832932a00886195ed9693
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-05-16 10:56:09 +0200

    Updated App Component to Include Username Input and Guess Location Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
-// import { useState } from 'react'
+import React, { useState } from 'react';
+import { GameData } from './types/types';
+import UsernameInput from './components/pages/UserComponents/UserNameInput';
+import GuessLocationPage from './components/pages/GameComponents/GuessLocationPage';
 
-function App() {
-  // const [count, setCount] = useState(0)
+const App: React.FC = () => {
+
+  const [gameData, setGameData] = useState<GameData | null>(null);
+  const [usernameInputVisible, setUsernameInputVisible] = useState<boolean>(true);
+  
+  const handleGameAvailable = (gameData: GameData, userData: string) => {
+    setGameData(gameData);
+    console.log(userData)
+    setUsernameInputVisible(false)
+  };
 
   return (
-    <>
-      <p>Hello, World!</p>
-    </>
-  )
-}
+    <div className="App">
+      {usernameInputVisible && <UsernameInput onGameAvailable={handleGameAvailable} />}
+      {gameData && (
+        <GuessLocationPage gameData={gameData} />
+      )}
+    </div>
+    
+  );
+};
 
 export default App
+
