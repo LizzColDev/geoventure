f6521cd9220aef545116fbe9d5b64b41480388fd
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-06-04 17:39:12 +0200

    implemented searchFamousPlace as a reusable function for game updates and removed getStreetViewImage in favor of direct API calls

--- backend/src/services/streetviewService.ts
+++ backend/src/services/streetviewService.ts
@@ -6,7 +6,7 @@ const GOOGLE_MAPS_API_KEY: string = process.env.GOOGLE_MAPS_API_KEY as string;
 
 
 // Function to search for famous places using Google Places API
-const searchFamousPlace = async (): Promise<StreetViewInfo[]> => {
+const searchFamousPlace = async (isGameUpdate: boolean): Promise<StreetViewInfo[]> => {
   try {
     const response = await axios.post(
       'https://places.googleapis.com/v1/places:searchText',
@@ -41,36 +41,4 @@ const searchFamousPlace = async (): Promise<StreetViewInfo[]> => {
   }
 }
 
-// Function to get a random Street View image of a famous place
-const  getStreetViewImage =async (): Promise<StreetViewInfo> => {
-  try {
-    const uniqueLocations = await searchFamousPlace();
-
-    const randomLocation = getRandomLocation(uniqueLocations);
-    const { initialLocation, namePlace } = randomLocation;
-    const { latitude, longitude } = initialLocation;
-    const size = '600x300'
-    const heading = '0';
-    const streetViewUrl = 
-      `https://maps.googleapis.com/maps/api/streetview?size=${size}` +
-      `&location=${latitude},${longitude}&heading=${heading}&key=${GOOGLE_MAPS_API_KEY}`;
-      
-    const response: AxiosResponse<ArrayBuffer> = await axios.get(streetViewUrl, { responseType: 'arraybuffer' });
-    const streetViewImage = `data:image/jpeg;base64,${Buffer.from(response.data).toString('base64')}`;
-      
-    if (response.status !== 200) {
-      throw new Error(`Error fetching StreetView image. Status code: ${response.status}`);
-    }
-    return { urlImage: streetViewImage, initialLocation: initialLocation, namePlace: namePlace };
-
-  } catch (error:unknown) {
-    if (axios.isAxiosError(error)) {
-      console.error('Error fetching StreetView image:', error.message);
-    } else {
-      console.error('Unknown error:', error);
-    }
-    throw new Error('Error fetching StreetView image.');
-  }
-}
-
-export { getStreetViewImage };
+export { searchFamousPlace };
