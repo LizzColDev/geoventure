97468f8bfa07ec384bd0c46ef704b4b3fcff89e1
Lisbeth Quintero <lizzcoldev@gmail.com>
2024-06-05 13:44:19 +0200

    Cleanup imports and simplify searchFamousPlace function

--- backend/src/services/streetviewService.ts
+++ backend/src/services/streetviewService.ts
@@ -1,12 +1,11 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { Coordinates, StreetViewInfo } from "../types";
-import { getRandomLocation } from "../utils/location/generatedRandomLocation";
 
 const GOOGLE_MAPS_API_KEY: string = process.env.GOOGLE_MAPS_API_KEY as string;
 
 
 // Function to search for famous places using Google Places API
-const searchFamousPlace = async (isGameUpdate: boolean): Promise<StreetViewInfo[]> => {
+const searchFamousPlace = async (): Promise<StreetViewInfo[]> => {
   try {
     const response = await axios.post(
       'https://places.googleapis.com/v1/places:searchText',
@@ -32,7 +31,6 @@ const searchFamousPlace = async (isGameUpdate: boolean): Promise<StreetViewInfo[
       initialLocation: place.location,
       namePlace: place.displayName.text
     })) || [];
-
     return famousPlaces;
 
   } catch (error: unknown) {
